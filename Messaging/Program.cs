c4ad5ff787b4ad98df69d63e6a0c5c7c4ebb298b
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-02-10 22:13:59 +0100

    1st

---
+++ Messaging/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> digits = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string someText = Console.ReadLine();
+            List<char> text = someText.ToList();
+            List<int> sumDigits = new List<int>();
+            int sum = 0;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < digits.Count; i++)
+            {
+                while (digits[i] > 0)
+                {
+                    sum += digits[i] % 10;
+                    digits[i] /= 10;
+                }
+                sumDigits.Add(sum);
+                sum = 0;
+            }
+            int searchedIndex = 0;
+            for (int i = 0; i < sumDigits.Count; i++)
+            {
+                if (text.Count < sumDigits[i])
+                {
+                    searchedIndex = sumDigits[i] - text.Count;
+                    sb.Append(text[searchedIndex]);
+                    text.RemoveAt(searchedIndex);
+                }
+                else
+                {
+                    searchedIndex = sumDigits[i];
+                    sb.Append(text[searchedIndex]);
+                    text.RemoveAt(searchedIndex);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
